452c1dd28705d4a5902046410f923efe23112d26
Gita Intan <pemburusurya@gmail.com>
2025-03-08 03:40:42 +0800

    Update after-install-linux.sh

--- after-install-linux.sh
+++ after-install-linux.sh
@@ -121,6 +121,16 @@ conda create -n myenv python=3.9 -y
 echo "Mengaktifkan lingkungan virtual..."
 conda activate myenv
 
+# Hapus file resolv.conf
+sudo rm /etc/resolv.conf
+
+# Buat file resolv.conf baru dan tambahkan nameserver
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
+
+# Lock file resolv.conf agar tidak bisa diubah
+sudo chattr +i /etc/resolv.conf
+
 echo "================================================"
 echo "Instalasi selesai!"
 echo "- Docker dan Docker Compose telah diinstal."
